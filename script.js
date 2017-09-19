73de467a1d92abcf9144407c419eac8d7c33d242
Andrzej <andy-26@wp.pl>
2017-09-19 14:52:23 +0200

    Slider

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+//     $(".btn1").click(function () {
+//         $('.btn1').hide(500);
+//         $('.btn2').show(500);
+//         $('.btn3').show(500);
+//     });
+//     $('.btn2').click(function () {
+//         $('.btn2').hide(500);
+//         $('.btn1').show(500);
+//         $('.btn3').show(500);
+//
+//     });
+//     $('.btn3').click(function () {
+//         $('.btn3').hide(500);
+//         $('.btn2').show(500);
+//         $('.btn1').show(500);
+//     });
+// //
+// // });
+//     $('.btn1').click(function () {
+//         $("#slide1").animate({width: 'toggle'}, 350);
+//     })
+//     $('.btn2').click(function () {
+//         $("#slide2").animate({width: 'toggle'}, 350);
+//     })
+//     $('.btn3').click(function () {
+//         $("#slide3").animate({width: 'toggle'}, 350);
+//     });
+//
+// });
+//
